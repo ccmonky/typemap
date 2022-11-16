919d37bf453ceabd70bc6cadbd78435b58c60177
yflau <yfliu-xjtu@qq.com>
2022-11-16 11:13:12 +0800

    chore

---
+++ const.go
@@ -0,0 +1,6 @@
+package typemap
+
+import "time"
+
+// NoExpiration for use with functions that take an expiration time.
+const NoExpiration time.Duration = -1
--- typemap.go
+++ typemap.go
@@ -8,25 +8,16 @@ import (
 	"reflect"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/eko/gocache/v3/cache"
 	"github.com/eko/gocache/v3/store"
 )
 
-const (
-	// For use with functions that take an expiration time.
-	NoExpiration time.Duration = -1
-	// For use with functions that take an expiration time. Equivalent to
-	// passing in the same expiration duration as was given to New() or
-	// NewFrom() when the cache was created (e.g. 5 minutes.)
-	DefaultExpiration time.Duration = 0
-)
-
 // RegisterType register a *Type into global TypeMap, if exists return error
 // - can specify TypeId with `WithTypeId`, or will use the GetTypeId(T) as default
 // - can specify instances cache container with `WithInstancesCache`, which use `github.com/eko/gocache` interface
-//   default to `cache.New(mapStore)`
+//   default to `cache.New[T](NewMap())`
+// - can specify T's dependencies(a slice of TypeId) with `WithDependencies`
 func RegisterType[T any](opts ...TypeOption) error {
 	typ := &Type{
 		typeId: GetTypeId[T](),
@@ -43,7 +34,8 @@ func RegisterType[T any](opts ...TypeOption) error {
 // SetType register a *Type into global TypeMap, if exists then override it
 // - can specify TypeId with `WithTypeId`, or will use the GetTypeId(T) as default
 // - can specify instances cache container with `WithInstancesCache`, which use `github.com/eko/gocache` interface
-//   default to `cache.New(mapStore)`
+//   default to `cache.New(NewMap())`
+// - can specify T's dependencies(a slice of TypeId) with `WithDependencies`
 func SetType[T any](opts ...TypeOption) error {
 	typ := &Type{
 		typeId: GetTypeId[T](),
@@ -69,7 +61,7 @@ func Types() map[string]*Type {
 	return typeMap.types
 }
 
-// GetType get *Type from global TypeMap
+// GetType get *Type corresponding to T from global TypeMap
 func GetType[T any](opts ...TypeOption) *Type {
 	typ := &Type{
 		typeId: GetTypeId[T](),
@@ -86,7 +78,7 @@ func getType(typeId string) *Type {
 	return typeMap.types[typeId]
 }
 
-type Type struct { // FIXME: 带泛型参数？
+type Type struct {
 	typeId         string
 	instancesCache any // cache.CacheInterface: cannot use generic type cache.CacheInterface[T any] without instantiation
 	dependencies   []string
@@ -134,25 +126,28 @@ func (typ *Type) UnmarshalJSON(b []byte) error {
 // Options control option func for TypeMap's type api, RegisterType|SetType|GetType
 type TypeOption func(*Type)
 
+// WithInstancesCache control option to specify the T's instances cache
 func WithInstancesCache[T any](cache cache.CacheInterface[T]) TypeOption {
 	return func(typ *Type) {
 		typ.instancesCache = cache
 	}
 }
 
+// WithTypeId control option to specify the T's TypeId, means do not use the default `GetTypeId(T)`
 func WithTypeId(typeId string) TypeOption {
 	return func(typ *Type) {
 		typ.typeId = typeId
 	}
 }
 
+// WithDependencies control option to specify the T's dependencies, should be a slice of valid TypeId, used for sort
 func WithDependencies(dependencies []string) TypeOption {
 	return func(typ *Type) {
 		typ.dependencies = dependencies
 	}
 }
 
-// Get get instance from Type's instances cache
+// Get get instance of T from Type's instances cache
 func Get[T any](ctx context.Context, key any, opts ...Option) (T, error) {
 	options := &Options{}
 	for _, opt := range opts {
@@ -176,7 +171,7 @@ func Register[T any](ctx context.Context, key any, object T, opts ...Option) err
 		return err
 	}
 	if _, err := cache.Get(ctx, key); err != nil {
-		if !errors.Is(err, store.NotFound{}) {
+		if !errors.Is(err, store.NotFound{}) { // NOTE: provide a Option to custom IsNotFound func?
 			return err
 		}
 	}
@@ -239,14 +234,17 @@ func (options *Options) Options() []Option {
 	return opts
 }
 
+// Option control option for instances api, Register|Set|Get|Delete|Clear
 type Option func(*Options)
 
+// WithTypeOption specify TypeOption as Option
 func WithTypeOption(typeOption TypeOption) Option {
 	return func(options *Options) {
 		options.TypeOptions = append(options.TypeOptions, typeOption)
 	}
 }
 
+// WithStoreOption specify store.Option as Option
 func WithStoreOption(storeOption store.Option) Option {
 	return func(options *Options) {
 		options.StoreOptions = append(options.StoreOptions, storeOption)
@@ -265,7 +263,7 @@ func getInstancesCache[T any](opts ...TypeOption) (cache.CacheInterface[T], erro
 	return cache, nil
 }
 
-// GetTypeId get type id of T
+// GetTypeId get TypeId of T
 func GetTypeId[T any]() string {
 	pointer := new(T)
 	typ := reflect.TypeOf(pointer)
