c79bda59b7ad4987ff45130e1ff4bbc05264507e
yflau <yfliu-xjtu@qq.com>
2022-12-22 14:52:27 +0800

    opt test

--- reg_test.go
+++ reg_test.go
@@ -9,8 +9,8 @@ import (
 )
 
 type App struct {
-	Switch    typemap.Reg[bool]                `json:"switch,omitempty"`
-	Blacklist typemap.Reg[map[string]struct{}] `json:"blacklist,omitempty"`
+	Switch    *typemap.Reg[bool]                `json:"switch,omitempty"`
+	Blacklist *typemap.Reg[map[string]struct{}] `json:"blacklist,omitempty"`
 }
 
 func TestReg(t *testing.T) {
@@ -57,5 +57,37 @@ func TestReg(t *testing.T) {
 		if _, ok := blacklist[item]; !ok {
 			t.Fatal("should contain")
 		}
+	}
+}
+
+func TestRegNull(t *testing.T) {
+	data := []byte(`{
+		"switch": {
+			"name": "switch",
+			"value": true
+		}
+	}`)
+	app := &App{}
+	err := json.Unmarshal(data, app)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if app.Switch.Value != true {
+		t.Fatal("should==")
+	}
+	if app.Blacklist != nil {
+		t.Fatal("should == nil")
+	}
+	ctx := context.Background()
+	s, err := typemap.Get[bool](ctx, "switch")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s != true {
+		t.Fatal("should==")
+	}
+	_, err = typemap.Get[map[string]struct{}](ctx, "blacklist-not-defined")
+	if err == nil {
+		t.Fatal("should not found")
 	}
 }
