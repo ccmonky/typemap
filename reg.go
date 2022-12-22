cb32fad301269a8cdd0db8352bda3a530a02a83e
yflau <yfliu-xjtu@qq.com>
2022-12-22 18:51:57 +0800

    make reg name optional

--- reg.go
+++ reg.go
@@ -9,8 +9,8 @@ import (
 
 // Reg used as a field that will set its name & value into instances map of T in typemap
 type Reg[T any] struct {
-	Name  string `json:"name"`
-	Value T      `json:"value"`
+	Name  *string `json:"name,omitempty"`
+	Value T       `json:"value"`
 	// Action is the action used to set intance of T, available values are: ["register", "set"], default is "set"
 	Action string `json:"action,omitempty"`
 }
@@ -31,20 +31,22 @@ func (r *Reg[T]) UnmarshalJSON(b []byte) error {
 	if err != nil {
 		return fmt.Errorf("register type of Reg[%T] failed: %v", *new(T), err)
 	}
-	switch strings.ToLower(r.Action) {
-	case "register":
-		err = Register(ctx, r.Name, r.Value)
-	default:
-		err = Set(ctx, r.Name, r.Value)
-	}
-	if err != nil {
-		return fmt.Errorf("set Reg[%T] %s failed: %v", *new(T), string(b), err)
+	if r.Name != nil {
+		switch strings.ToLower(r.Action) {
+		case "register":
+			err = Register(ctx, *r.Name, r.Value)
+		default:
+			err = Set(ctx, *r.Name, r.Value)
+		}
+		if err != nil {
+			return fmt.Errorf("set Reg[%T] %s failed: %v", *new(T), string(b), err)
+		}
 	}
 	return nil
 }
 
 type regSerdeHelper[T any] struct {
-	Name   string `json:"name"`
-	Value  T      `json:"value"`
-	Action string `json:"action,omitempty"`
+	Name   *string `json:"name,omitempty"`
+	Value  T       `json:"value"`
+	Action string  `json:"action,omitempty"`
 }
